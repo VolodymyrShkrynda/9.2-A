2a99242af57d4e3d4d8bbfcb8d5c12719bc08c69
VolodymyrShcrynda <volodymyr.shkrynda.itis.2020@lpnu.ua>
2020-12-23 11:36:32 +0200

    first commit

---
+++ 9.2(B)/9.2(A).cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 9.2(A)/lab 9.2(A).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest92A
+{
+	TEST_CLASS(UnitTest92A)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Student* p = new Student[1];
+			p->prizv = "Matus";
+			p->inform = 5;
+			p->math = 4;
+			p->physics = 3;
+
+			bool functionResult = BinSearch(p, 1, "Matus", 3, 4);
+
+			Assert::AreEqual(false, functionResult);
+		}
+	};
+}
